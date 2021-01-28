e39fcea0f175a1f4fe47909598dfd08cfa554d05
shaik1729 <shaiktajuddinshavali@gmail.com>
2021-01-28 22:12:31 +0530

    inital commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var test = document.getElementById('parent');
+var data = [1,2,3,4,5,6];
+    
+for (var i = 0; i < data[i]; i++)
+{
+test.innerHTML += '<div class="message"><img src="../images/b' + data[i] +'.png" alt=""><h2 style="color: rgb(7, 25, 212); margin-top: -300px; margin-left: 30px;">text</h2></div>';
+} 

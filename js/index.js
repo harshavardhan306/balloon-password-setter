9d141ea02b1a3a1da1e7566fb116fde7979d7737
shaik1729 <shaiktajuddinshavali@gmail.com>
2021-01-29 21:57:19 +0530

    frontend all done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,45 @@
 var test = document.getElementById('parent');
-var data = [1,2,3,4,5,6];
-    
-for (var i = 0; i < data[i]; i++)
-{
-test.innerHTML += '<div class="message"><img src="../images/b' + data[i] +'.png" alt=""><h2 style="color: rgb(7, 25, 212); margin-top: -300px; margin-left: 30px;">text</h2></div>';
-} 
+var randomchar = new Array(50);
+var bcount = 0;
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+var password = null;
+
+
+for (var i = 0; i < 50; i++) {
+    randomchar[i] = possible.charAt(Math.floor(Math.random() * possible.length));
+}
+
+for (var i = 0; i < 50; i++) {
+
+    if (bcount == 7) {
+        bcount = 0;
+    }
+    if (bcount == 0) {
+        bcount += 1;
+    }
+    test.innerHTML += '<div class="message" id = "balloon' + i + '" onClick="drop_and_getval(id,\'' + randomchar[i] + '\')"><img src="../images/b' + bcount + '.png" alt=""><h2 style="color: rgb(7, 25, 212); margin-top: -300px; margin-left: 50px;">' + randomchar[i] + '</h2></div>';
+    bcount += 1;
+}
+
+let count = 0;
+function drop_and_getval(idOfImageDiv, str1) {
+    if (count == 7) {
+        alert("click ok to submit password");
+        window.location.href = "../html_files/welcome.html";
+    }
+    if (typeof (str1) == "number") {
+        str1 = String(str1);
+    }
+
+    if (password == null) {
+        password = str1;
+    }
+    else {
+        password = password + str1;
+    }
+    let elem = document.getElementById(idOfImageDiv);
+    elem.parentNode.removeChild(elem);
+    count++;
+}
+
+console.log("password = " + password);
